35ceeb0eccb6ec65dd3cddc8994a24c7ed37feaa
Charlie Somerville <charlie@charliesomerville.com>
2013-10-19 14:46:36 -0400

    test app boots in less than a second

---
+++ test/integration/meta_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class MetaTest < MiniTest::Unit::TestCase
+  def test_app_boots_in_less_than_a_second
+    bm = Benchmark.measure do
+      `bin/rails runner 0`
+    end
+
+    assert bm.real < 1
+  end
+end
