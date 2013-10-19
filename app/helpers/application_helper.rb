b13cc52b973f159b5f0fb4604aba1dda0f87eaf2
Simon Eskildsen <sirup@sirupsen.com>
2013-10-19 11:28:11 -0400

    Refactor task badge into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 require 'pry'
 
 module ApplicationHelper
+  def task_badge(task)
+    if completed_task?(task) 
+      "<span class='label label-success'>Passed</span>"
+    elsif attempted_task?(task)
+      "<span class='label label-warning'>Attempted</span>"
+    end
+  end
+
+  private
   def completed_task?(task)
     task.passed?(current_user) if current_user
   end
