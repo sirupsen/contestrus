a65233f9906beb92d2c692d4c0c757de3231f155
Charlie Somerville <charlie@charliesomerville.com>
2013-10-19 11:51:12 -0400

    fuck off rubygems

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,12 @@
+$LOADED_FEATURES << "rubygems"
+
 module Kernel
   if method_defined?(:gem_original_require)
     alias_method :require, :gem_original_require
+  end
+
+  def gem(*)
+    # no-op
   end
 end
 
