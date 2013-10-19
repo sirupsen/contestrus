2e7a40e01a3b20b118afdf63572fcbd670ea41b9
Simon Eskildsen <sirup@sirupsen.com>
2013-10-19 12:00:17 -0400

    Fix schema verison

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014210210) do
+ActiveRecord::Schema.define(version: 20131019155640) do
+
   create_table "competitions", force: true do |t|
     t.string   "name"
     t.datetime "start_at"
