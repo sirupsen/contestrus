22c654b5d37ead802f32e2878f76952422fdf52e
Simon Eskildsen <sirup@sirupsen.com>
2013-10-19 14:47:35 -0400

    Add training badge

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "#time_left_badge on an open competition returns nil" do
-    refute time_left_badge(competitions(:open))
+    assert_match /Training/, time_left_badge(competitions(:open))
   end
 
   test "#time_left_badge for an ongoing competition returns time remaining" do
