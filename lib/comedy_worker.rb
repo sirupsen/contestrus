e2a01d1422299d1f0f91a47915b10372d110114f
Charlie Somerville <charlie@charliesomerville.com>
2013-10-20 00:47:40 -0400

    stop comedy worker when puma restarts

--- lib/comedy_worker.rb
+++ lib/comedy_worker.rb
@@ -2,11 +2,23 @@ require "thread"
 
 ComedyWorker = Queue.new
 
+def ComedyWorker.working?
+  @working
+end
+
+def ComedyWorker.stop
+  @working = false
+  self << :kill
+end
+
 def ComedyWorker.work!
+  @working = true
   Thread.start do
-    loop do
+    while working?
       begin
-        ComedyWorker.deq.perform
+        job = ComedyWorker.deq
+        break if job == :kill
+        job.perform
       rescue Exception => e
         $stderr.puts "Exception in background job! #{e}: #{e.message}\n#{e.backtrace.join("\n")}"
       end
