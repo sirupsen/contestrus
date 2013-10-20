a11c614ef70775f491bd4015808dd58ebcfecab5
Charlie Somerville <charlie@charliesomerville.com>
2013-10-20 01:53:07 -0400

    fiddle with puma settings a little

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,8 @@ environment "production"
 pidfile "/app/shared/puma.pid"
 state_path "/app/shared/puma.state"
 stdout_redirect "/app/shared/log/puma_stdout", "/app/shared/log/puma_stderr", true
-threads 8, 16
+threads 3
+workers 3
 
 on_restart do
   ComedyWorker.stop
