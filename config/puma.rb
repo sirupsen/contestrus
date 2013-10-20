f693512296c79946cb810d22f63d5f7376923208
Charlie Somerville <charlie@charliesomerville.com>
2013-10-20 02:28:17 -0400

    just kill normally with USR2

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,7 @@
-activate_control_app "unix:///app/shared/pumactl.sock"
 bind "unix:///app/shared/puma.sock"
 daemonize true
 directory "/app/current"
 environment "production"
 pidfile "/app/shared/puma.pid"
-state_path "/app/shared/puma.state"
 stdout_redirect "/app/shared/log/puma_stdout", "/app/shared/log/puma_stderr", true
 threads 4, 8
-
-on_restart do
-  ComedyWorker.stop
-end
