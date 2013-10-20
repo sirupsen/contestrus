8702065eb60e6cbcd31e9404ac9bc63ccf97def3
Charlie Somerville <charlie@charliesomerville.com>
2013-10-20 00:33:52 -0400

    drop delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019195956) do
+ActiveRecord::Schema.define(version: 20131020043322) do
 
   create_table "competitions", force: true do |t|
     t.string   "name"
@@ -20,22 +20,6 @@ ActiveRecord::Schema.define(version: 20131019195956) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "evaluations", force: true do |t|
     t.text     "body"
