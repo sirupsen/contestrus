22236dd64dd84e80afc14852a4ad55dc5bdc3875
Charlie Somerville <charlie@charliesomerville.com>
2013-10-19 22:23:41 -0400

    delete sirup user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-sirup = User.create!(username: "sirup", password: "seekrit", admin: true, email: 'whoever@example.com')
 YAML.load_file("./test/fixtures/languages.yml").each do |key, value|
   Language.create!(value)
 end
