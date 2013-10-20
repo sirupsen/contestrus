2cc002feb3be3332733c7003c58660d16832dfdb
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-10-20 18:19:12 -0400

    Switch to Pypy

--- app/models/language/python.rb
+++ app/models/language/python.rb
@@ -1,6 +1,6 @@
 Language.define "Python" do |lang|
   lang.extension = "py"
-  lang.image = "bouk/python"
-  lang.build = "python -c \"from subprocess import check_output;import py_compile;py_compile.compile('/sandbox/file.py',cfile=check_output(['mktemp']).strip(), doraise=True)\""
-  lang.run = "python /sandbox/file.py"
+  lang.image = "bouk/pypy"
+  lang.build = "pypy -c \"from subprocess import check_output;import py_compile;py_compile.compile('/sandbox/file.py',cfile=check_output(['mktemp']).strip(), doraise=True)\""
+  lang.run = "pypy /sandbox/file.py"
 end
