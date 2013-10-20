459106b849dab497dc4c25132df3d3e627f1b9e7
Charlie Somerville <charlie@charliesomerville.com>
2013-10-20 02:26:01 -0400

    just kill normally with USR2

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,8 @@
-activate_control_app "unix:///app/shared/pumactl.sock"
 bind "unix:///app/shared/puma.sock"
 daemonize true
 directory "/app/current"
 environment "production"
 pidfile "/app/shared/puma.pid"
-state_path "/app/shared/puma.state"
 stdout_redirect "/app/shared/log/puma_stdout", "/app/shared/log/puma_stderr", true
 threads 4, 8
 
