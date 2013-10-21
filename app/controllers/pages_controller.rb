70734e523080639ff2f29759a6218e4410410528
Simon Eskildsen <sirup@sirupsen.com>
2013-10-20 22:27:50 -0400

    Allow user to see instructions without authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_filter :require_user
+
   def instructions
     @languages = Language.languages.values
   end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
   test "should get instructions" do
-    sign_in
     get :instructions
     assert_response :success
   end
