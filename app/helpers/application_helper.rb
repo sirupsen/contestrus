73b24af17ba02143414451563b06045656863b91
Simon Eskildsen <sirup@sirupsen.com>
2013-10-21 00:28:28 -0400

    RemoveFix not attempted from sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,8 +20,11 @@ module ApplicationHelper
     end
   end
 
-  def task_status_badge(task, user = current_user)
-    "<span class='label label-#{task_status_class(task, user)}'>#{task_status_human(task, user)}</span>".html_safe
+  def task_status_badge(task, user = current_user, show_not_attempted = false)
+    human_status = task_status_human(task, user)
+    return nil if human_status == "Not attempted" && !show_not_attempted 
+
+    "<span class='label label-#{task_status_class(task, user)}'>#{human_status}</span>".html_safe
   end
 
   def render_markdown(markdown)
--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,16 +1,2 @@
 module CompetitionsHelper
-  def task_status_class(task, user = current_user)
-    case task_status_human(task, user)
-    when "Passed"
-      "success"
-    when "Attempted"
-      "warning"
-    when "Not attempted"
-      "info"
-    end
-  end
-
-  def task_status_badge(task, user = current_user)
-    "<span class='label label-#{task_status_class(task, user)}'>#{task_status_human(task, user)}</span>".html_safe
-  end
 end
