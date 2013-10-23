5f8c04123d56d7cac6725ef82260ae896b0bf6f1
Simon Eskildsen <sirup@sirupsen.com>
2013-10-22 23:18:50 -0400

    move mathjax to secure cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require mathjax
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
